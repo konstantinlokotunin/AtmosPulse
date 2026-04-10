e5ce113447bd07ee3b4aeb1af93f05f77a444d25
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-10 12:34:13 +0200

    Refactor plot_weather to use x-ticks for dates

--- src/visualization.py
+++ src/visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
@@ -6,15 +7,19 @@ plt.style.use("seaborn-v0_8")
 def plot_weather(dates, temp_max, temp_min):
     _, ax = plt.subplots(figsize=(10, 5))
 
-    ax.plot(dates, temp_max, label="Max Temperature", marker='o', color="#eb2556")
-    ax.plot(dates, temp_min, label="Min Temperature", marker='o', color="#3b82f6")
+    x = np.arange(len(dates))
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(dates, fontsize=10, rotation=30)
+
+    ax.plot(x, temp_max, label="Max Temperature", marker='o', color="#eb2556")
+    ax.plot(x, temp_min, label="Min Temperature", marker='o', color="#3b82f6")
 
     ax.set_title("Temperature Trends (Vienna)", fontsize=16, pad=12)
     ax.set_ylabel("°C")
-    ax.set_xticklabels(dates, fontsize=10, rotation=30)
 
     ax.legend()
-    ax.grid(True, color="#e1e8f4", linestyle='--', linewidth=0.5, alpha=0.6)
+    ax.grid(True, color="#e1e8f4", linestyle='--', linewidth=1, alpha=0.6)
 
     plt.tight_layout()
     plt.show()
