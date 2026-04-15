8367b88ceca8f4dc7ca79e6555f8f48e996e7cf8
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-15 19:12:39 +0200

    Enhance visualization styles and y-tick adjustments

--- src/visualization.py
+++ src/visualization.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set_theme(style="whitegrid", context="talk")
+sns.despine()
+sns.set_palette("deep")
 
 def plot_weather(df):
     _, ax = plt.subplots(figsize=(10, 5))
@@ -12,20 +14,20 @@ def plot_weather(df):
     ax.set_xticks(range(len(df)))
     ax.set_xticklabels(df["date_str"], fontsize=11, rotation=30)
 
-    ax.plot(x, df["temp_max"], label="Max Temperature °C", marker='o', linewidth=2, color="#eb2556")
-    ax.plot(x, df["temp_min"], label="Min Temperature °C", marker='o', linewidth=2, color="#3b82f6")
+    ax.plot(x, df["temp_max"], label="Max Temperature °C", marker='o', linewidth=2)
+    ax.plot(x, df["temp_min"], label="Min Temperature °C", marker='o', linewidth=2)
 
-    ax.set_title("Daily Temperature °C (Vienna)", fontsize=17, pad=12)
+    ax.set_title("Daily Temperature °C (Vienna)", fontsize=17, pad=12, weight="bold")
     ax.set_ylabel("°C")
 
     y_min = int(df["temp_min"].min())
     y_max = int(df["temp_max"].max())
-    ax.set_yticks(np.arange(y_min, y_max + 1, 3))
+    ax.set_yticks(np.arange(y_min, y_max + 2, 3))
 
     ax.tick_params(axis='y', labelsize=11)
 
-    ax.legend()
-    ax.grid(True, linestyle='--', alpha=0.6)
+    ax.legend(frameon=False)
+    ax.grid(True, linestyle='--', alpha=0.5)
 
     plt.tight_layout()
     plt.savefig("outputs/weather_data.png", dpi=300)
@@ -36,9 +38,9 @@ def plot_precipitation(df):
 
     width = 0.35
 
-    ax.bar(df["date"], df["precipitation"], width, color="#60a5fa")
+    ax.bar(df["date"], df["precipitation"], width)
 
-    ax.set_title("Daily Precipitation (Vienna)", fontsize=17, pad=12)
+    ax.set_title("Daily Precipitation (Vienna)", fontsize=17, pad=12, weight="bold")
     ax.set_ylabel("mm")
 
     y_max = int(df["precipitation"].max()) + 2
@@ -47,7 +49,8 @@ def plot_precipitation(df):
 
     ax.tick_params(axis='y', labelsize=11)
 
-    ax.grid(True, linestyle='--', alpha=0.6)
+    ax.legend(frameon=False)
+    ax.grid(True, linestyle='--', alpha=0.5)
 
     plt.xticks(fontsize=11, rotation=30)
     plt.tight_layout()
@@ -68,22 +71,22 @@ def plot_temperature_with_ma(df):
         anomalies["date"],
         anomalies["temp_avg"],
         color="#7d0926",
-        label="Anomalies",
+        label="Heat peaks and cold dips",
         zorder=3
     )
 
-    ax.set_title("Temperature °C Trends (Vienna)", fontsize=17, pad=12)
+    ax.set_title("Temperature °C Trends (Vienna)", fontsize=17, pad=12, weight="bold")
     ax.set_ylabel("°C")
 
     y_min = int(df["temp_avg"].min())
     y_max = int(df["temp_avg"].max())
 
-    ax.set_yticks(np.arange(y_min, y_max + 1))
+    ax.set_yticks(np.arange(y_min, y_max + 2))
     
     ax.tick_params(axis='y', labelsize=11)
 
-    ax.legend()
-    ax.grid(True, linestyle='--', alpha=0.6)
+    ax.legend(frameon=False)
+    ax.grid(True, linestyle='--', alpha=0.5)
 
     plt.xticks(fontsize=11, rotation=30)
     plt.tight_layout()
